e350c169c539b20aa48e128b0e132251280d016c
Alexander K. Hudek <alex@diligenceengine.com>
2012-09-13 19:49:21 -0400

    Updated to Clojure 1.4 and added a few comments.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-ua "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :description "Parse user agents."
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :main clj-ua.core)
--- src/clj_ua/core.clj
+++ src/clj_ua/core.clj
@@ -1,6 +1,5 @@
 (ns clj-ua.core
-  (:require [clojure.contrib.command-line :as ccl]
-            [clojure.string :as string]))
+  (:require [clojure.string :as string]))
 
 (import java.util.Arrays)
 
@@ -9,15 +8,15 @@
 (defrecord UserAgent [#^Browser browser
                       #^Platform platform])
 
-(defn- log [data]
+(def unknown-user-agent
+  (UserAgent. (Browser. "unknown" "unknown") (Platform. "unknown" "unknown")))
+
+(defn- log 
+  [data]
   (prn (merge {:ns "clj-ua"} data)))
 
-(defn- get-user-agent [browser platform]
-  (UserAgent.
-    (Browser. (:name browser) (:version browser))
-    (Platform. (:name platform) (:version platform))))
-
-(defn- parse-version-from-comment [comment]
+(defn- parse-version-from-comment 
+  [comment]
   (let [version (re-find #"([0-9]+)[[_.]([A-Za-z0-9]+)]+" comment)]
     (if (nil? version)
       ; then
@@ -27,20 +26,25 @@
       ; else
       (.replaceAll (nth version 0) "_" "."))))
 
-(defn- handle-comment [parts]
+(defn- handle-comment 
+  [parts]
   (if (and (not (nil? parts)) (< 0 (alength parts)))
     (let [part (nth parts 0)]
       (if (< 1 (.length part))
-        (apply array-map [:name part :version (parse-version-from-comment part)])
+        (Platform. part (parse-version-from-comment part))
         (handle-comment (Arrays/copyOfRange parts 1 (count parts)))))))
 
-(defn- parse-browser [seq]
+(defn- parse-browser 
+  "Get the browser information from the parsed user agent string."
+  [seq]
   (let [part (last seq)]
     (if (or (nil? part) (.equals (nth part 0) ""))
       (parse-browser (subvec seq 0 (- (count seq) 1))) ;subvec is exclusive of the end
-      (apply array-map [:name (nth part 1) :version (nth part 3)]))))
+      (Browser. (nth part 1) (nth part 3)))))
 
-(defn- parse-platform [seq]
+(defn- parse-platform 
+  "Get the platform information from the parsed user agent string."
+  [seq]
   (let [part (first seq)]
     (if (< 6 (count part))
       ; then
@@ -53,14 +57,15 @@
       ; else
       (parse-platform (rest seq)))))
 
-(defn parse [#^String agent]
-  (log {:fn "parse" :agent agent})
+(defn parse 
+  "Parse a user agent string and return a UserAgent record containing browser
+   and platform entries."
+  [#^String agent]
+  #_(log {:fn "parse" :agent agent})
   (if (and (not (nil? agent)) (not (.equals agent "")))
-    (let [seq (vec (re-seq #"([^/\s]*)(/([^\s]*))?(\s*\[[a-zA-Z][a-zA-Z]\])?\s*(\((([^()]|(\([^()]*\)))*)\))?\s*" agent))
-          browser (parse-browser seq)
-          platform (parse-platform seq)]
-      (get-user-agent browser platform))
-    (get-user-agent {:name "unknown" :version "unknown"} {:name "unknown" :version "unknown"})))
+    (let [seq (vec (re-seq #"([^/\s]*)(/([^\s]*))?(\s*\[[a-zA-Z][a-zA-Z]\])?\s*(\((([^()]|(\([^()]*\)))*)\))?\s*" agent))]
+      (UserAgent. (parse-browser seq) (parse-platform seq)))
+    unknown-user-agent))
 
 (defn -main [& args]
   (if args
